b96baec542f1d7c7286d4cb05c53e09550cc0058
Luiz Matheus <luizm7@gmail.com>
2019-04-22 00:58:54 -0300

    build do app na pasta public

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>     
+       
+    </head>
+    <body>
+
+     <app-root></app-root>
+    <script type="text/javascript" src="{{ asset('js/runtime.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/es2015-polyfills.js') }}" nomodule></script>
+    <script type="text/javascript" src="{{ asset('js/polyfills.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/styles.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+    </body>
+</html>
