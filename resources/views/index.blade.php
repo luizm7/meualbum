9407405a27fa9b93f615d99ca34f6895daa6f412
Luiz Matheus <luizm7@gmail.com>
2019-04-24 00:28:18 -0300

    Pagina principal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <base href="/">
-        <title>Laravel</title>     
+        <title>Laravel</title>  
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">   
        
     </head>
     <body>
