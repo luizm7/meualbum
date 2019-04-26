f56ea05f5b2520f787d53220380e900b269ec4b5
Luiz Matheus <luiz.fonseca@credicom.com.br>
2019-04-26 17:01:02 -0300

    Inclusao das variaveis e migration

--- app/Http/Controllers/PostControlador.php
+++ app/Http/Controllers/PostControlador.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class PostControlador extends Controller
 {
     /**
@@ -13,7 +15,7 @@ class PostControlador extends Controller
      */
     public function index()
     {
-        //
+        return Post::all();
     }
 
     /**
@@ -34,7 +36,17 @@ class PostControlador extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $post = new Post();
+
+        $path - $request->file(arquivo)->store('imagens','public');
+
+        $post->nome = $request->nome;
+        $post->email = $request->email;
+        $post->titulo = $request->titulo;
+        $post->subtitulo = $request->subtitulo;
+        $post->mensagem = $request->mensagem;
+        $post->path = $path;
+        $post->like = 0;
     }
 
     /**
